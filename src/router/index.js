93cb5686223cac037b31f648b6274ddebd0c505a
xvehai028 <43464493+xvehai028@users.noreply.github.com>
2024-12-20 15:10:32 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+//import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 import routes from "@/router/routes";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  //history: createWebHashHistory(),//哈希路由
+  history: createWebHistory(process.env.BASE_URL), // 使用history模式
   routes,
 });
 
